368fcf81db734f5f3f0ab1a9f1abdadc15e8f4ca
Jitendra Prajapat <jps007@ymail.com>
2026-01-04 12:32:03 +0000

    I hope this is last one

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mindverse-portal/', // This MUST match your repository name exactly
+  base: './', // <--- This dot is the magic fix. It forces relative paths.
 })
